9eff37f2a967b3b11c53bf6270b1a9c2bfda9d56
VishalYadav26 <vishalyadav26092002@gmail.com>
2025-09-18 02:04:36 +0530

    backend connected

--- src/app/feedback.service.ts
+++ src/app/feedback.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FeedbackService {
-  private apiUrl = "https://yourapp.onrender.com/api/feedback";
+  private apiUrl = "https://angular-backend-feedback.onrender.com/api/feedback";
 
   constructor(private http: HttpClient) {}
 
